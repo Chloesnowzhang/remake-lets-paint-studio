6445553c949d7e0224f9ce8589f0ae3f37824fbe
Chloe Zhang <chloeassessment@gmail.com>
2021-11-14 16:32:43 +1100

    Make .name ellipse in >= 577 vp and adjust Navbar CSS

--- src/components/displays/navbarMenu/navbar/Navbar.js
+++ src/components/displays/navbarMenu/navbar/Navbar.js
@@ -7,32 +7,28 @@ import Hamburger from '../../../utils/hamburger/Hamburger';
 const Navbar = ({ extended, setExtended }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.left}>
-        <div className={styles.hamburger}>
-          <Hamburger extended={extended} setExtended={setExtended} />
-        </div>
-        <div className={styles.name}>Let's Paint Studio</div>
+      <div className={styles.hamburger}>
+        <Hamburger extended={extended} setExtended={setExtended} />
       </div>
+      <div className={styles.name}>Let's Paint Studio</div>
 
-      <div className={styles.menu}>
+      <div className={styles.menuItem}>
         <span>
-          <span>
-            <FontAwesomeIcon icon="sticky-note" />
-          </span>
-          <span>Get Quote</span>
+          <FontAwesomeIcon icon="sticky-note" />
         </span>
+        <span>Get Quote</span>
+      </div>
+      <div className={styles.menuItem}>
         <span>
-          <span>
-            <FontAwesomeIcon icon="phone-alt" />
-          </span>
-          <span>Call Now</span>
+          <FontAwesomeIcon icon="phone-alt" />
         </span>
+        <span>Call Now</span>
+      </div>
+      <div className={styles.menuItem}>
         <span>
-          <span>
-            <FontAwesomeIcon icon="directions" />
-          </span>
-          <span>Get Directions</span>
+          <FontAwesomeIcon icon="directions" />
         </span>
+        <span>Get Directions</span>
       </div>
     </div>
   );
